02d100f2d1a04a988d775a617cc1f526be22ed46
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2022-09-17 11:24:15 +0100

    basic development setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve, dirname } = require("path");
+const path = require(`path`);
+
+module.exports = {
+	entry: "./src/index.js",
+
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, `dist`),
+	},
+	mode: `development`,
+	devtool: `inline-source-map`,
+	devServer: {
+		static: `./dist`,
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [`style-laoder`, `css-loader`],
+			},
+			{
+				test: /\.(jpg|jpeg|svg|png|gif)$/i,
+				type: `asset/resource`,
+			},
+		],
+	},
+};
